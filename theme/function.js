1b493068acad872b0a88ae1c75880cb4431dd741
Tuanlailaptrinh <87818411+Tuanlailaptrinh@users.noreply.github.com>
2022-04-22 05:55:58 -0700

    Add files via upload

---
+++ theme/function.js
@@ -0,0 +1,20 @@
+console.log(
+    "%c Website: ThaiTuan.Tech %c",
+    'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;',
+    "font-size:12px;color:#999999;"
+);
+console.log(
+    "%c Code By Thái Tuấn %c",
+    'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;',
+    "font-size:12px;color:#999999;"
+);
+console.log(
+    "%c Contact: https://www.facebook.com/nguyenhuynhthaituan.dev/ %c",
+    'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;',
+    "font-size:12px;color:#999999;"
+);
+console.log(
+    "%c Contact: https://zalo.me/0935042064 %c",
+    'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;',
+    "font-size:12px;color:#999999;"
+);
